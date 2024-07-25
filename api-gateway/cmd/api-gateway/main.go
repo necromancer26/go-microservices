ae6023ea902f711fabc738abd48ed6da1af2f88a
khalil.chettaoui <khalil.chettaoui@orbem.ai>
2024-07-25 18:42:25 +0200

    api gateway auth

--- api-gateway/cmd/api-gateway/main.go
+++ api-gateway/cmd/api-gateway/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/necromancer26/go-microservices/api-gateway/config"
 	"github.com/necromancer26/go-microservices/api-gateway/internal/handlers"
 	"github.com/necromancer26/go-microservices/api-gateway/pkg/logger"
 )
+
+func JwtVerify(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header missing", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.Split(authHeader, " ")[1]
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte("secret"), nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	config.LoadConfig()
